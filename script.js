b758048293f12386832098ebe0af2b744bbc31cc
sameh <sameh080081@gmail.com>
2022-06-27 17:18:41 +0400

    update script file

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ class storage {
     static addBook(book){
         const bookList = storage.getBooks();
         bookList.push(book);
-        localStorage.setItem('booList',JSON.stringify(bookList));
+        localStorage.setItem('bookList',JSON.stringify(bookList));
     }
 
     static removeBook(title,author){
@@ -29,14 +29,33 @@ class storage {
                 bookList.splice(index,1);
             }
         });
-        localStorage.setItem('booList',JSON.stringify(bookList));
+        localStorage.setItem('bookList',JSON.stringify(bookList));
+    }
+}
+
+class Interface {
+    static showBooks(){
+    const list = document.querySelector('.list');
+    const book = document.createElement('tr');
+    const bookList = storage.getBooks();
+    bookList.forEach((book) => {
+    book.innerHTML = `
+    <td>${book.title}</td>
+    <td>by</td>
+    <td>${book.author}</td>
+    <button class= 'delete' >Remove</button>
+    `;
+    list.appendChild(book);
+    })
     }
 }
 
 const form = document.querySelector('booksinput')
 form.addEventListener('submit', (e) => {
-    e.preventDefault()
-    const title = document.querySelector('.title').value
-    const author = document.querySelector('.author').value
-    const book = new Books(title, author)
-})
+    e.preventDefault();
+    const title = document.querySelector('.title').value;
+    const author = document.querySelector('.author').value;
+    const book = new Books(title, author);
+    storage.addBook(book);
+    Interface.showBooks();
+});
