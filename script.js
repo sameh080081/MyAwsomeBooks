40ccc57e54c03638aa1e48fcf2c0af2234c86714
sameh <sameh080081@gmail.com>
2022-06-28 07:58:50 +0400

    fix linters errors

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
-function Books(title,author){
-    this.title = title;
-    this.author = author;
+function Books(title, author){
+  this.title = title;
+  this.author = author;
 }
 
 function getBooks(){
   let bookList;
   if(JSON.parse(localStorage.getItem('bookList')===null)){
-      bookList = [];
+    bookList = [];
   }
   else {
-      bookList = JSON.parse(localStorage.getItem('bookList'));
+    bookList = JSON.parse(localStorage.getItem('bookList'));
   }
    return bookList;
 }
 
 function addBook(book){
-   const bookList = getBooks();
-   bookList.push(book);
-   localStorage.setItem('bookList',JSON.stringify(bookList));
+  const bookList = getBooks();
+  bookList.push(book);
+  localStorage.setItem('bookList',JSON.stringify(bookList));
 }
 
-function removeBook(title,author){
+function removeBook(title, author){
   const bookList = getBooks();
   const newList=bookList.filter(item => item.title !== title && item.author !== author);
   localStorage.setItem('bookList',JSON.stringify(newList));
@@ -39,11 +39,11 @@ function showBook(book){
   const list = document.querySelector('.list');
   const bookrecord = document.createElement('tr');
   bookrecord.innerHTML = `
-   <td>${book.title}</td>
-   <td>&nbsp</td>
-   <td>${book.author}</td>
-   <button class= 'delete' >Remove</button>
-   `;
+  <td>${book.title}</td>
+  <td>&nbsp</td>
+  <td>${book.author}</td>
+  <button class= 'delete' >Remove</button>
+  `;
   list.appendChild(bookrecord);
 }
 
