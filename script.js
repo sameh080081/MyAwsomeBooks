5f9fcc38d1768a4c7365a60ba2ba331346e2685f
sameh <sameh080081@gmail.com>
2022-06-29 15:07:48 +0400

    fix linters

--- script.js
+++ script.js
@@ -12,21 +12,22 @@ function Book(title, author) {
 
 class Interface {
   static displayBook() {
-    allBooks.innerHTML = ``;
-      storage.forEach(book => {
-        const bookAbove = document.createElement('div');
-          bookAbove.classList.add('class-book');
-          const bookAboveText = document.createElement('p');
-          bookAboveText.textContent = `"${book.title}" by ${book.author}`;
-          const bookAboveBtn = document.createElement('button');
-          bookAboveBtn.textContent = 'Remove';
-          bookAboveBtn.classList.add('remove');
-          bookAboveBtn.setAttribute('id', book.id);
-          bookAbove.append(bookAboveText);
-          bookAbove.append(bookAboveBtn);
-          allBooks.append(bookAbove);
-        })
+    allBooks.innerHTML = '';
+    storage.forEach((book) => {
+      const bookAbove = document.createElement('div');
+      bookAbove.classList.add('class-book');
+      const bookAboveText = document.createElement('p');
+      bookAboveText.textContent = `"${book.title}" by ${book.author}`;
+      const bookAboveBtn = document.createElement('button');
+      bookAboveBtn.textContent = 'Remove';
+      bookAboveBtn.classList.add('remove');
+      bookAboveBtn.setAttribute('id', book.id);
+      bookAbove.append(bookAboveText);
+      bookAbove.append(bookAboveBtn);
+      allBooks.append(bookAbove);
+    });
   }
+
   static removeBook(targetId) {
     const newBookList = storage.filter(book => book.id !== targetId);
     storage.length = 0;
@@ -34,6 +35,7 @@ class Interface {
     localStorage.setItem('form', JSON.stringify(storage))
     Interface.displayBook();
   }
+  
   static appendBook(theBook) {
     storage.push(theBook);
     localStorage.setItem('form', JSON.stringify(storage));
