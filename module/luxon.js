a9304152c0796aba09b59bc65e08c81584607f72
sameh <sameh080081@gmail.com>
2022-07-06 00:44:01 +0400

    fix linters

--- module/luxon.js
+++ module/luxon.js
@@ -1,5 +1,5 @@
-// these aren't really private, but nor are they really useful to document
 /* eslint no-use-before-define: 0 */  // --> OFF
+// these aren't really private, but nor are they really useful to document
 /**
  * @private
  */
